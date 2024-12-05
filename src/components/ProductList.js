f0f65db2b0e4efe1b96bc1b874484a7a89237f01
Quique9723 <enriquefdez97@gmail.com>
2024-12-05 12:44:08 +0100

    maquetando estilos

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
+
 import {
   InstantSearch,
   SearchBox,
   Hits,
   Highlight,
-} from 'react-instantsearch-dom';
-import { algoliaConfig } from '../algoliaConfig';
+} from "react-instantsearch-dom";
+import { algoliaConfig } from "../algoliaConfig";
 
 const ProductList = () => {
   // Render de cada producto
   const Product = ({ hit }) => (
     <div className="product-card">
       <img src={hit.image} alt={hit.name} className="product-image" />
-      <h3><Highlight attribute="name" hit={hit} /></h3>
+      <h3>
+        <Highlight attribute="name" hit={hit} />
+      </h3>
       <p>${hit.price}</p>
     </div>
   );
@@ -23,7 +26,12 @@ const ProductList = () => {
         searchClient={algoliaConfig.searchClient}
         indexName={algoliaConfig.indexName}
       >
-        <SearchBox />
+        <SearchBox
+          translations={{
+            placeholder: "Busca productos...",
+          }}
+          className="search-box"
+        />
         <div className="hits-container">
           <Hits hitComponent={Product} />
         </div>
